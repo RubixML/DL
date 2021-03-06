eb9c0bcf5f3c07ffc7a4569cd30a1a3ee42d45b8
Andrew DalPino <me@andrewdalpino.com>
2021-03-06 16:19:36 -0600

    Remove unnecessary files

--- ext/rubix_dl.h
+++
@@ -1,9 +0,0 @@
-
-/* This file was generated automatically by Zephir do not modify it! */
-
-#ifndef ZEPHIR_CLASS_ENTRIES_H
-#define ZEPHIR_CLASS_ENTRIES_H
-
-#include "rubix/dl/poc.zep.h"
-
-#endif
